ff726c3ec051c1015d72ff2e19051d1b10819850
wangzz2019 <jack.wang@datadoghq.com>
2021-01-07 21:29:52 +0900

    add progressbar

--- helloworld/GTDetailViewController.m
+++ helloworld/GTDetailViewController.m
@@ -10,20 +10,33 @@
 
 @interface GTDetailViewController ()<WKNavigationDelegate>
 @property(nonatomic,strong,readwrite) WKWebView *webView;
+@property(nonatomic,strong,readwrite) UIProgressView *progressView;
 @end
 
 @implementation GTDetailViewController
+
+- (void)dealloc{
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.view addSubview:({
-        self.webView=[[WKWebView alloc] initWithFrame:CGRectMake(0, 80, self.view.frame.size.width, self.view.frame.size.height-88)];
+        self.webView=[[WKWebView alloc] initWithFrame:CGRectMake(0, 90, self.view.frame.size.width, self.view.frame.size.height-88)];
         self.webView.navigationDelegate=self;
         self.webView;
     })];
-    
+    [self.view addSubview:({
+        self.progressView = [[UIProgressView alloc] initWithFrame:CGRectMake(0, 90, self.view.frame.size.width,20)];
+        self.progressView;
+    })];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org"]]];
+    
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    
+//    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org"]]];
 }
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation{
@@ -33,4 +46,10 @@
 //    NSLog(@"");
     decisionHandler(WKNavigationActionPolicyAllow);
 }
+
+- (void)observeValueForKeyPath:(nullable NSString *)keyPath ofObject:(nullable id)object change:(nullable NSDictionary<NSKeyValueChangeKey, id> *)change context:(nullable void *)context{
+    NSLog(@"");
+    self.progressView.progress=self.webView.estimatedProgress;
+}
+
 @end
